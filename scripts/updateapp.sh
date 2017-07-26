7cc0075d6e2dc777aec4f46746355b9de6729a4c
shoulhafiz <mnorhafiz@gmail.com>
2017-07-26 16:30:47 +0800

    new init script to change waagent.conf to enable swap

--- scripts/updateapp.sh
+++ scripts/updateapp.sh
@@ -1,1 +1,12 @@
+#!/bin/bash -x
+
+SED=/usr/bin/sed
+
+$SED 's/ResourceDisk.EnableSwap=n/ResourceDisk.EnableSwap=y/g' < /etc/waagent.conf > /etc/waagent-temp.conf
+$SED 's/ResourceDisk.SwapSizeMB=0/ResourceDisk.SwapSizeMB=2048/g' < /etc/waagent-temp.conf > /etc/waagent.conf
+
+rm /etc/waagent-temp.conf
+
+systemctl restart waagent
+
 exit 0
