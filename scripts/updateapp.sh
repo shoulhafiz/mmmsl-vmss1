58c38e1b0b997d708e5069fc40acdcd153a34343
shoulhafiz <mnorhafiz@gmail.com>
2017-07-28 15:55:33 +0800

    update 00-mpm.conf

--- scripts/updateapp.sh
+++ scripts/updateapp.sh
@@ -3,10 +3,23 @@
 SED=/usr/bin/sed
 
 $SED 's/ResourceDisk.EnableSwap=n/ResourceDisk.EnableSwap=y/g' < /etc/waagent.conf > /etc/waagent-temp.conf
-$SED 's/ResourceDisk.SwapSizeMB=0/ResourceDisk.SwapSizeMB=2048/g' < /etc/waagent-temp.conf > /etc/waagent.conf
+$SED 's/ResourceDisk.SwapSizeMB=0/ResourceDisk.SwapSizeMB=6144/g' < /etc/waagent-temp.conf > /etc/waagent.conf
 
-rm /etc/waagent-temp.conf
+rm -f /etc/waagent-temp.conf
 
 systemctl restart waagent
+
+echo "
+LoadModule mpm_prefork_module modules/mod_mpm_prefork.so
+<IfModule mpm_prefork_module>
+        ServerLimit             104
+        StartServers            30
+        MinSpareServers           20
+        MaxSpareServers          30
+        MaxRequestWorkers         100
+        MaxConnectionsPerChild   0
+</IfModule>
+" > /etc/httpd/conf.modules.d/00-mpm.conf
+systemctl restart httpd
 
 exit 0
